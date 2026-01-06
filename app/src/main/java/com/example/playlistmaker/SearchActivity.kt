a8951a4473cadd054ce6e345803f15fb2f43a95c
Ivan Piskanaft <cambogiyt@gmail.com>
2026-01-06 11:02:29 +0300

    homework sprint 9 done

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,13 +1,82 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
 
 class SearchActivity : AppCompatActivity() {
+    private val toolbar: MaterialToolbar by lazy { findViewById(R.id.toolbar) }
+    private val searchInputLayout: TextInputLayout by lazy { findViewById(R.id.search_input_layout) }
+    private val searchInput: TextInputEditText by lazy { findViewById(R.id.search_input) }
+    private var searchInputText = ""
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_search)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search)) { view, insets ->
+            val statusBar = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            view.updatePadding(top = statusBar.top)
+            insets
+        }
+        setTextWatcher()
+        setListeners()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_INPUT_TEXT, searchInputText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val gotText = savedInstanceState.getString(SEARCH_INPUT_TEXT, TEXT_DEF)
+        searchInput.setText(gotText)
+    }
+
+    companion object {
+        private const val SEARCH_INPUT_TEXT = "SEARCH_INPUT_TEXT"
+        private const val TEXT_DEF = ""
+    }
+
+    private fun setTextWatcher() {
+        searchInputLayout.isEndIconVisible = false
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchInputLayout.isEndIconVisible = !s.isNullOrEmpty()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                searchInputText = s.toString()
+
+            }
+        }
+
+        searchInput.addTextChangedListener(simpleTextWatcher)
+    }
+
+    private fun setListeners() {
+        toolbar.setNavigationOnClickListener { finish() }
+        searchInputLayout.setEndIconOnClickListener {
+            searchInput.text?.clear()
+            searchInput.clearFocus()
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(searchInput.windowToken, 0)
+        }
 
     }
 }
+
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,15 +1,24 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.ImageView
+
+import android.widget.TextView
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
 
+
 class SettingsActivity : AppCompatActivity() {
+    private val toolbar: MaterialToolbar by lazy { findViewById(R.id.toolbar) }
+    private val shareButton: TextView by lazy { findViewById(R.id.action_share_app) }
+    private val supportButton: TextView by lazy { findViewById(R.id.action_contact_support) }
+    private val agreementButton: TextView by lazy { findViewById(R.id.action_user_agreement) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -19,7 +28,40 @@ class SettingsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+
+
+        setListeners()
+    }
+
+    private fun setListeners() {
         toolbar.setNavigationOnClickListener { finish() }
+
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.settings_share_app_content))
+
+            startActivity(
+                Intent.createChooser(
+                    intent, getString(R.string.settings_share_app_chooser_title)
+                )
+            )
+        }
+
+        supportButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.settings_support_email)))
+            intent.putExtra(
+                Intent.EXTRA_SUBJECT, getString(R.string.settings_support_email_subject)
+            )
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.settings_support_email_body))
+            startActivity(intent)
+        }
+
+        agreementButton.setOnClickListener {
+            val intent =
+                Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.settings_agreement_url)))
+            startActivity(intent)
+        }
     }
 }
